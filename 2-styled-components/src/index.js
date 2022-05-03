9be06c5deb1ca2d8e499d11361577d2b7a00447e
Danush <Danush@litmus7.com>
2022-05-03 09:39:54 +0530

    Implemented css in js
    customized header using css in js

--- 2-styled-components/src/index.js
+++ 2-styled-components/src/index.js
@@ -1,8 +1,24 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import styled from "styled-components"
 
+
+const Header= styled.h1`
+color:red;
+
+@media(max-width:480px){
+  font-size:24px;
+  color:green;
+}
+`
+
+const Hero= styled(Header)`
+color:blue;
+font-size:100px`
 const App = () => {
-  return <h1>Hello React!</h1>;
+  return <><Hero>I am Hero</Hero>
+  <Header>Hello React!</Header>
+  </>
 };
 
 const rootElement = document.getElementById("root");
